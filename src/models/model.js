dc0c9affe721752abf5a3c5d335636b624b8ffdd
cdebost <corentin@montagestudio.com>
2017-01-28 12:45:04 -0800

    Add generic data model constructor

--- src/models/model.js
+++ src/models/model.js
@@ -1,4 +1,11 @@
 export default class Model {
 
+    constructor(data) {
+        for (let i in data) {
+            if (data.hasOwnProperty(i)) {
+                this[i] = data[i]
+            }
+        }
+    }
 }
 
