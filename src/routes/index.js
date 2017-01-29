a694131b661302b7d2bec7bffc6a99844634cc31
cdebost <corentin@montagestudio.com>
2017-01-28 16:38:42 -0800

    Add body parser

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@
 import api from './api'
 import express from 'express'
 import path from 'path'
+import bodyParser from 'body-parser'
 
 export default ({app, config}) => {
+    app.use(bodyParser.json())
     app.use('/api', api({config}))
     app.use(express.static(path.join(__dirname, '../../public')))
 }
