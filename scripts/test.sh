cd123b0e4ca75d03a31b99f540639f7578045517
cdebost <corentin@montagestudio.com>
2017-03-03 12:28:26 -0800

    Move DB fixture data to test directory

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 echo 'Populating test data'
-mongoimport --db zotplan_test --collection departments --drop --file db/fixtures/departments-dataset.json
-mongoimport --db zotplan_test --collection courses     --drop --file db/fixtures/courses-dataset.json
-mongoimport --db zotplan_test --collection users       --drop --file db/fixtures/users-dataset.json
-mongoimport --db zotplan_test --collection plans       --drop --file db/fixtures/plans-dataset.json
+mongoimport --db zotplan_test --collection departments --drop --file test/datasets/departments.json
+mongoimport --db zotplan_test --collection courses     --drop --file test/datasets/courses.json
+mongoimport --db zotplan_test --collection users       --drop --file test/datasets/users.json
+mongoimport --db zotplan_test --collection plans       --drop --file test/datasets/plans.json
 
 echo 'Running test'
 ENVIRONMENT=test istanbul cover _mocha -- --reporter spec --compilers js:babel-register --require babel-polyfill --recursive ./test
